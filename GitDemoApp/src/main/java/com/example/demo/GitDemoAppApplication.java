58e50da12edbb91b8f47fa1421aa63d99233323c
Administrator <Administrator@Lalasa>
2022-06-14 15:44:41 +0530

    second commit

--- GitDemoApp/src/main/java/com/example/demo/GitDemoAppApplication.java
+++ GitDemoApp/src/main/java/com/example/demo/GitDemoAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub");
+		System.out.println("Welcome to GitHub");
 		SpringApplication.run(GitDemoAppApplication.class, args);
 	}
 
